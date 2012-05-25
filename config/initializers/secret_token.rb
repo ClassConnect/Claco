a90bca9d1ce18cf6d3a0eed2dd18d5878ecda8e6
Eric Simons <ericsimons@es40.net>
2012-05-24 20:36:12 -0700

    a whole new world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Claco::Application.config.secret_token = 'a1511db368e7805756df8b109a1c9a89cd5f14ac7ee286175454f821bcdc5502329b161240bc1c4711db3ace2a10ce17f3343acede177cc1e1821c53f9446245'

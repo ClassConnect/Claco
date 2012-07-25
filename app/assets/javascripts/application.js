467ed8cab3362baea4181dce72009d0b306bea12
Eric Simons <ericsimons@es40.net>
2012-07-25 12:20:38 -0700

    frontend plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,57 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+
+$(document).ready(function() {
+	$(".noclose").on("click", function(e){
+	    e.stopPropagation();
+	});
+
+	$("#top-notch-btn").on("click", function(e){
+		if (!$(".noclose").is(":visible")) {
+
+			setTimeout(function() {$(".login-focus").focus();},100);
+
+		}
+	    
+	});
+
+});
+
+
+
+
+function initAsyc(content) {
+	$('.async-pop').html(content).slideDown(100);
+}
+
+function destroyAsyc() {
+	$('.async-pop').slideUp(100);
+}
+
+
+
+
+
+
+function htmlEncode(value){
+    if (value) {
+        return jQuery('<div />').text(value).html();
+    } else {
+        return '';
+    }
+}
+ 
+function htmlDecode(value) {
+    if (value) {
+        return $('<div />').html(value).text();
+    } else {
+        return '';
+    }
+}
+
+
+// close facebox
+function closefBox() {
+	jQuery(document).trigger('close.facebox');
+}

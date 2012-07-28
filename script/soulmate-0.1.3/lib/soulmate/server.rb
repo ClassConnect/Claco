850e3d93344e544889887693c2b1cb56677e7523
Matt Frisbie <msfrisbie@gmail.com>
2012-07-28 12:18:40 -0700

    eric is a butthead

--- script/soulmate-0.1.3/lib/soulmate/server.rb
+++ script/soulmate-0.1.3/lib/soulmate/server.rb
@@ -31,7 +31,7 @@ module Soulmate
       types.each do |type|
         matcher = Matcher.new(type)
 	#matcher.matches_for_term(term, :limit => limit).each { |result| smushset << "{ label: \"#{ result['data']['label'] }\", value: \"#{ result['data']['value'] }\" }" }
-	matcher.matches_for_term(term, :limit => limit).each { |result| smushset << { :label => result['data']['label'], :value => result['data']['value'] } }
+	matcher.matches_for_term(term, :limit => limit).each { |result| smushset << { :title => result['data']['label'], :label => result['data']['value'] } }
       end
 
       #smushset.uniq.each { |result| retstr += "#{result}," }

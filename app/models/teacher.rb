8f97ab93f6ae9026d18d1875483152bef1cdb759
Albert Pai <iamalbertpai@gmail.com>
2012-08-18 20:03:59 -0700

    initial omniauth setup

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,7 +4,7 @@ class Teacher
 	# Include default devise modules. Others available are:
 	# :token_authenticatable, :confirmable,
 	# :lockable, :timeoutable and :omniauthable
-	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :authentication_keys => [:login]
 
 	## Database authenticatable
 	field :email,              :type => String, :null => false, :default => "", :unique => true

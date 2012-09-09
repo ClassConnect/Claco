3a1c0a3e7df8ccaf6d55c1c45491f5ad03f36531
Albert Pai <iamalbertpai@gmail.com>
2012-09-08 17:59:57 -0700

    remove auth team

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	before_filter :authenticate_teacher!, :except => [:index, :autocomplete, :tos, :privacy, :about, :united]
+	before_filter :authenticate_teacher!, :except => [:index, :autocomplete, :tos, :privacy, :about, :united, :team]
 
 	def index
 		@title = "Claco"

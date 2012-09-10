7329053119be4a35ebddc41ad751d93f917e3ce9
Albert Pai <iamalbertpai@gmail.com>
2012-09-10 13:56:23 -0700

    asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ Claco::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  config.assets.enabled = true
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
@@ -43,7 +44,7 @@ Claco::Application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//static.cla.co"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
    config.assets.precompile += ['*.js', '*.css', '*.png', '*.jpg']

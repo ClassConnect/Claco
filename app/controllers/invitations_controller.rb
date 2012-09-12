72dd696446fd5d2d4eb1288f26c27371d23afdda
Albert Pai <iamalbertpai@gmail.com>
2012-09-11 20:46:30 -0700

    binder createfile callback fix

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,6 +2,8 @@ class InvitationsController < ApplicationController
 	before_filter :authenticate_teacher!
 
 	def invite
+		@invitations = Invitation.where(:from => current_teacher.id.to_s).sort_by{|i| i.submitted}
+
 		render "invite", :layout => false
 	end
 

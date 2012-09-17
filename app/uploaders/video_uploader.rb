96ba48dba2e920aca6c93efe859a39df366b9998
Albert Pai <iamalbertpai@gmail.com>
2012-09-16 22:21:34 -0700

    zen test

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,7 +1,12 @@
 class VideoUploader < DataUploader
 
-	def filename
-		"vid.mp4"
-	end
+  def url
+    if fog_public
+      fog_uri = CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), "#{store_dir}/vid.mp4").public_url
+    else
+      fog_uri = CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), "#{store_dir}/vid.mp4").authenticated_url
+    end
+    fog_uri
+  end
 
 end

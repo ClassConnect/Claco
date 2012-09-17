1ca60bded203cc5fc76f9c871332bd0a056c170a
Albert Pai <iamalbertpai@gmail.com>
2012-09-16 22:53:39 -0700

    zen test

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -4,8 +4,17 @@ class VideoUploader < DataUploader
     if fog_public
       fog_uri = CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), "#{store_dir}/vid.mp4").public_url
     else
-      fog_uri = CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), "#{store_dir}/vid.mp4").authenticated_url
+      fog_uri = CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), "#{store_dir}/vid.mp4").authenticated_url.sub(/https:\/\/#{self.fog_directory}.s3.amazonaws.com/, self.fog_host)
     end
+    fog_uri
+  end
+
+  def posterurl
+  	if fog_public
+      fog_uri = CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), "#{store_dir}/poster.png").public_url
+	else
+      fog_uri = CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), "#{store_dir}/poster.png").authenticated_url.sub(/https:\/\/#{self.fog_directory}.s3.amazonaws.com/, self.fog_host)
+	end
     fog_uri
   end
 

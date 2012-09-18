2280535d21885ea4abc65e66c3252c9f0790e89b
Albert Pai <iamalbertpai@gmail.com>
2012-09-17 17:29:40 -0700

    zen test

--- app/controllers/zencoder_callbacks_controller.rb
+++ app/controllers/zencoder_callbacks_controller.rb
@@ -119,7 +119,7 @@ class ZencoderCallbacksController < ApplicationController
 			stathash[:imgfile][:retrieved] = true
 
 			# Binder.delay(:queue => 'thumbgen').get_thumbnail_from_url(binder.id, binder.current_version.video.posterurl)
-			Binder.get_thumbnail_from_url(binder.id, binder.current_version.video.posterurl)
+			Binder.get_thumbnail_from_url(binder.id.to_s, binder.current_version.video.posterurl)
 
 		end
 

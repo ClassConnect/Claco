7097a84739be0ef11618c45f0dff3973487fe176
Albert Pai <iamalbertpai@gmail.com>
2012-09-20 11:51:48 -0700

    blitz route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -240,6 +240,10 @@ class HomeController < ApplicationController
 		render "public/tos.html"#, :status => 200 and return
 	end
 
+	def blitz
+		render :text => "42"
+	end
+
 	def search
 
 		if params[:q].present?
--- config/routes.rb
+++ config/routes.rb
@@ -112,6 +112,8 @@ Claco::Application.routes.draw do
 
 	post	'/zcb'																					=> 'zencoder_callbacks#processed'
 
+	get		'/mu-43c0e7df-28b26f3a-b2c0e5a5-6cdca93a'												=> 'home#blitz' #Blitz
+
 	constraints(:username => /[^\/]+/, :root => /[^\/]+/, :title => /[^\/]+/, :format => /json|html/) do
 
 		#Subscribe/unsubscribe

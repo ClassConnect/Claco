3ea9391591cdfb1a91edae8e7d9eb02df2b40e0e
Albert Pai <iamalbertpai@gmail.com>
2012-10-14 16:50:31 -0700

    changed routing for spaces
    Signed-off-by: Albert Pai <iamalbertpai@gmail.com>

--- app/controllers/pioneers_controller.rb
+++ app/controllers/pioneers_controller.rb
@@ -39,7 +39,7 @@ protected
 
 	def pioneer_route(binder)
 		if binder.class == Binder && binder.parent["id"] == Setting.f("pioneer").v
-			return "/pioneers/#{CGI.escape(binder.title.gsub("+", "-"))}/#{binder.id.to_s}"
+			return "/pioneers/#{CGI.escape(binder.title.gsub(" ", "-"))}/#{binder.id.to_s}"
 		end
 	end
 

49bdf08766c6975f9c7a86e8d5abb5983eaaa582
Albert Pai <iamalbertpai@gmail.com>
2012-10-14 11:22:23 -0700

    remove authentication on goodies page
    Signed-off-by: Albert Pai <iamalbertpai@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	before_filter :authenticate_teacher!, :except => [:index, :autocomplete, :tos, :privacy, :about, :united, :team, :pioneers, :pioneersshow]
+	before_filter :authenticate_teacher!, :except => [:index, :autocomplete, :tos, :privacy, :about, :united, :team, :pioneers, :pioneersshow, :goodies]
 
 	def index
 		@title = "Claco"

0b4cf0b301056186e328044261e7996eff5b0c70
Diwank Singh <diwank.singh@gmail.com>
2012-10-25 12:35:25 -0700

    FIx routes
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -243,9 +243,9 @@ class HomeController < ApplicationController
 		# expires_in 1.hour
 	end
 
-	def explore
-		@title = "Explore"
-	end
+	# def explore
+	# 	@title = "Explore"
+	# end
 
 	def goodies
 		@title = "Goodies"
@@ -263,9 +263,9 @@ class HomeController < ApplicationController
 		# expires_in 1.hour
 	end
 
-	def explore_detail
-		@title = "Explore Detail"
-	end
+	# def explore_detail
+	# 	@title = "Explore Detail"
+	# end
 
 	def privacy
 		@title = "Privacy Policy"
